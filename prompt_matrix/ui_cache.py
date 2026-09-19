7e11ce5e3d3f2e55c8b8b7c24be7e07ae5829214
OG <orhangorenn@gmail.com>
2026-09-19 19:18:47 +0300

    fix(signin): minimal shell chrome for /signin, /signup pages

--- prompt_matrix/ui_cache.py
+++ prompt_matrix/ui_cache.py
@@ -1,6 +1,6 @@
 """Cache-bust query strings. Change these, not scattered HTML literals."""
 
-APP_CSS = "assure-149"
-APP_JS = "assure-149"
+APP_CSS = "assure-150"
+APP_JS = "assure-150"
 LANDING_CSS = "59"
 LANDING_JS = "45"
